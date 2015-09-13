7771a74537e8f416491142014a04ef71b04458da
Skullmasher <skullmasher@heartlessgaming.com>
2015-09-13 23:20:43 +0000

    Setting my age dynamically

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -11,15 +11,31 @@ jQuery(document).ready(function() {
 
   // Set viewport width & height on window resize
   $(window).resize(function(){
+
     $('.slide').width( $(window).width() );
     $('.slide').height( $(window).height() );
+
   });
+
+  // Getting my age dynamically
+  var myAge = function() {
+    
+    var myBirthday = new Date('March 18, 1992 00:45:00');
+    var today = new Date();
+
+    return ( today.getFullYear() - myBirthday.getFullYear() );
+
+  };
+  
+  $('.skullAge').append( myAge() + ' ans.' );
 
   $('.grid').masonry({
     // options
     itemSelector: '.grid-item',
     columnWidth: 200
   });
+
+
 
 });
 
