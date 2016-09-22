e17c767a5dc7fc610326a25c66b84782d3c6ee94
skullmasher <skullmasher@heartlessgaming.com>
2016-09-22 16:54:26 +0200

    Adding build tools

--- index.php
+++ index.php
@@ -14,6 +14,8 @@
 
 get_header(); ?>
 
+  <h1>topkek !</h1>
+
   <div id="primary" class="content-area">
     <main id="main" class="site-main" role="main">
 
---
+++ js/script.js
@@ -0,0 +1,12 @@
+var greetingMessage = function () {
+  console.log('  #####   ')
+  console.log(' #######  ')
+  console.log('#  ###  # ' + ' Hello There')
+  console.log('#   #   # ')
+  console.log('######### ' + ' Come contribute to the code !')
+  console.log(' ### ###  ')
+  console.log('  #####   ' + ' github.com/heartless-gaming')
+  console.log('  # # #   ')
+}
+
+greetingMessage()
