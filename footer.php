40e16def6b55cd9a8e0e22617e276f21e895330e
skullmasher <skullmasher@heartlessgaming.com>
2016-10-20 22:35:34 +0200

    Refactored title color and weight

--- footer.php
+++ footer.php
@@ -11,13 +11,15 @@
 
 ?>
 </div><!-- #content -->
-<footer id="colophon" class="site-footer" role="contentinfo">
-  <p class="disclaimer"><strong>Skullmasher.io</strong> est une société basée en France affilié au régime de auto-entrepreneur. <strong>Numéro siren : 793553488</strong></p>
-	<div class="site-info">
-		<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'skullmasher-io' ) ); ?>"><?php printf( esc_html__( 'Proudly powered by %s', 'skullmasher-io' ), 'WordPress' ); ?></a>
-		<span class="sep"> | </span>
-		<?php printf( esc_html__( 'Theme: %1$s by %2$s.', 'skullmasher-io' ), 'skullmasher-io', '<a href="http://underscores.me/" rel="designer">Underscores.me</a>' ); ?>
-	</div><!-- .site-info -->
+<footer class="page-footer" role="contentinfo">
+  <div class="band--darkgrey">
+    <p class="disclaimer"><strong>Skullmasher.io</strong> est une société basée en France affilié au régime de auto-entrepreneur. <strong>Numéro siren : 793553488</strong></p>
+  	<div class="site-info">
+  		<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'skullmasher-io' ) ); ?>"><?php printf( esc_html__( 'Proudly powered by %s', 'skullmasher-io' ), 'WordPress' ); ?></a>
+  		<span class="sep"> | </span>
+  		<?php printf( esc_html__( 'Theme: %1$s by %2$s.', 'skullmasher-io' ), 'skullmasher-io', '<a href="http://underscores.me/" rel="designer">Underscores.me</a>' ); ?>
+  	</div>
+  </div>
 </footer><!-- #colophon -->
 
 <?php wp_footer(); ?>
