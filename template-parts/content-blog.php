57d7c2652a459d9e47dbb186a5ca7ce0a8b73d90
skullmasher <skullmasher@heartlessgaming.com>
2016-10-25 20:23:17 +0200

    Adding comment link and comment count

--- template-parts/content-blog.php
+++ template-parts/content-blog.php
@@ -8,11 +8,10 @@
  */
 
 ?>
-
-<article id="post-<?php the_ID(); ?>" <?php post_class('o-box post-list__item'); ?>>
-		<?php the_title( '<h2 class="post-list__title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h2>' ); ?>
-		<div class="post-list__meta flex-container">
+<article id="post-<?php the_ID(); ?>" <?php post_class('post-list__item'); ?>>
+    <?php the_title( '<h2 class="post-list__title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h2>' ); ?>
+    <div class="post-list__meta flex-container">
       <span class="post-list__publish-date"><?php skullmasher_io_post_publish_date(); ?></span>
-      <span class="post-list__comments-count">42 commentaires</span>
+      <a class="post-list__comments" href="<?php comments_link(); ?>"><?php comments_number( '', '1 commentaire', '% commentaires' ); ?></a>
     </div>
 </article>
