c9e9cc7202b684b9b57e1d8c44c95498cd6b30c3
skullmasher <skullmasher@heartlessgaming.com>
2016-11-05 02:50:09 +0100

    First responsive rule

--- functions.php
+++ functions.php
@@ -185,3 +185,7 @@ require get_template_directory() . '/inc/customizer.php';
  * Load Jetpack compatibility file.
  */
 require get_template_directory() . '/inc/jetpack.php';
+
+/**
+ * testing
+ */
