75006dd0b64544353355319dfb96530a26cf1967
skullmasher <skullmasher@heartlessgaming.com>
2016-11-05 17:23:26 +0100

    Put the content and the sidebar next to each other.

--- front-page.php
+++ front-page.php
@@ -26,7 +26,7 @@ $recent_posts = wp_get_recent_posts($recent_posts_args, OBJECT);
 
 get_header(); ?>
 <main role="main">
-  <div class="band band--lightgrey promotional-skills">
+  <div class="promotional-skills band band--lightgrey">
     <div class="promotional-skill">
       <header class="promotional-skill__header o-media">
         <?php echo file_get_contents(get_template_directory() .'/img/develop.svg') ?>
@@ -56,23 +56,25 @@ get_header(); ?>
       <p class="promotional-skill__text">Bien trop souvent des utilisateurs se retrouve seul face à la gestion de leur site web. Tous logiciel informatique à besoin d'une personne responsable de sa maintenance. Avant qu'il ne soit trop tard choisissez un responsable capable de vous diriger dans la conduite du changement de votre site web et assurer sa pérénité.</p>
     </div>
   </div>
-  <div class="last-articles">
-    <div class="last-articles__heading-container">
-      <h3 class="last-articles__heading">Les <?php echo $number_posts ?> derniers Articles</h3>
-      <span class="last-articles__heading-trail"></span>
+  <div class="content-sidebar">
+    <div class="last-articles">
+      <div class="last-articles__heading-container">
+        <h3 class="last-articles__heading">Les <?php echo $number_posts ?> derniers Articles</h3>
+        <span class="last-articles__heading-trail"></span>
+      </div>
+  <?php foreach( $recent_posts as $post ) :
+
+  setup_postdata($post);
+  if ($post->post_status === 'publish') {
+    get_template_part( 'template-parts/content-homepage', get_post_format() );
+  }
+
+  endforeach; wp_reset_postdata(); ?>
+      <a href="<?php echo get_permalink(get_page_by_title('blog')) ?>" class="btn btn--rounded btn--show-all-articles" role="button">Consulter tous les articles</a>
     </div>
-<?php foreach( $recent_posts as $post ) :
-
-setup_postdata($post);
-if ($post->post_status === 'publish') {
-  get_template_part( 'template-parts/content-homepage', get_post_format() );
-}
-
-endforeach; wp_reset_postdata(); ?>
-    <a href="<?php echo get_permalink(get_page_by_title('blog')) ?>" class="btn btn--rounded btn--show-all-articles" role="button">Consulter tous les articles</a>
+    <?php get_sidebar('homepage') ?>
   </div>
 </main>
 
 <?php
-get_sidebar('homepage');
 get_footer();
