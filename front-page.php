86b4ff9759e8ec361ba43a47740f71d0a0cf85a5
skullmasher <skullmasher@heartlessgaming.com>
2016-11-05 03:50:38 +0100

    :skull: Tablet responsive rules for the promotional-skills

--- front-page.php
+++ front-page.php
@@ -26,34 +26,34 @@ $recent_posts = wp_get_recent_posts($recent_posts_args, OBJECT);
 
 get_header(); ?>
 <main class="o-wrapper" role="main">
-  <div class="band band--lightgrey">
-    <div class="promotional-skills">
-      <header class="promotional-skills__header o-media">
+  <div class="band band--lightgrey flex-container">
+    <div class="promotional-skill">
+      <header class="promotional-skill__header o-media">
         <?php echo file_get_contents(get_template_directory() .'/img/develop.svg') ?>
-        <h2 class="promotional-skills__title o-media-content">Concevoir avec Wordpress</h2>
+        <h2 class="promotional-skill__title o-media-content">Concevoir avec Wordpress</h2>
       </header>
-      <p class="promotional-skills__text">Dans le monde de la gestion de contenu web, Wordpress est bien souvent la solution la plus maitrisée par les utilisateurs. Si à l'avenir votre site vitrine devient un site e-comerce, Wordpress répondra à ce besoin.</p>
+      <p class="promotional-skill__text">Dans le monde de la gestion de contenu web, Wordpress est bien souvent la solution la plus maitrisée par les utilisateurs. Si à l'avenir votre site vitrine devient un site e-comerce, Wordpress répondra à ce besoin.</p>
     </div>
-    <div class="promotional-skills">
-      <header class="promotional-skills__header o-media">
+    <div class="promotional-skill">
+      <header class="promotional-skill__header o-media">
         <?php echo file_get_contents(get_template_directory() .'/img/share.svg') ?>
-        <h2 class="promotional-skills__title o-media-content">Former et Partager</h2>
+        <h2 class="promotional-skill__title o-media-content">Former et Partager</h2>
       </header>
-      <p class="promotional-skills__text">Demander une formation sur votre solution Wordpress. Découvrez votre site Wordpress par la pratique. Le web est composé de technologie libre et open-source en son coeur, le partage des connaissances en fait partis intégrante. Retrouvez toutes les astuces et tutoriaux web sur le blog.</p>
+      <p class="promotional-skill__text">Demander une formation sur votre solution Wordpress. Découvrez votre site Wordpress par la pratique. Le web est composé de technologie libre et open-source en son coeur, le partage des connaissances en fait partis intégrante. Retrouvez toutes les astuces et tutoriaux web sur le blog.</p>
     </div>
-    <div class="promotional-skills">
-      <header class="promotional-skills__header o-media">
+    <div class="promotional-skill">
+      <header class="promotional-skill__header o-media">
         <?php echo file_get_contents(get_template_directory() .'/img/develop.svg') ?>
-        <h2 class="promotional-skills__title o-media-content">Développer</h2>
+        <h2 class="promotional-skill__title o-media-content">Développer</h2>
       </header>
-      <p class="promotional-skills__text">Adaptation des maquettes aux écran responsive pour refleter au mieux la vision du web designer. Dévelopement back-office avec PHP et NodeJS. Application iOS et Android hybride avec Cordova (Phonegap). Instalation et déploiement de votre site web en Python, Ruby, PHP, NodeJS sur Linux. Contactez moi pour dicuter de votre projet.</p>
+      <p class="promotional-skill__text">Adaptation des maquettes aux écran responsive pour refleter au mieux la vision du web designer. Dévelopement back-office avec PHP et NodeJS. Application iOS et Android hybride avec Cordova (Phonegap). Instalation et déploiement de votre site web en Python, Ruby, PHP, NodeJS sur Linux. Contactez moi pour dicuter de votre projet.</p>
     </div>
-    <div class="promotional-skills">
-      <header class="promotional-skills__header o-media">
+    <div class="promotional-skill">
+      <header class="promotional-skill__header o-media">
         <?php echo file_get_contents(get_template_directory() .'/img/server.svg') ?>
-        <h2 class="promotional-skills__title o-media-content">Maintenir des Solutions Web</h2>
+        <h2 class="promotional-skill__title o-media-content">Maintenir des Solutions Web</h2>
       </header>
-      <p class="promotional-skills__text">Bien trop souvent des utilisateurs se retrouve seul face à la gestion de leur site web. Tous logiciel informatique à besoin d'une personne responsable de sa maintenance. Avant qu'il ne soit trop tard choisissez un responsable capable de vous diriger dans la conduite du changement de votre site web et assurer sa pérénité.</p>
+      <p class="promotional-skill__text">Bien trop souvent des utilisateurs se retrouve seul face à la gestion de leur site web. Tous logiciel informatique à besoin d'une personne responsable de sa maintenance. Avant qu'il ne soit trop tard choisissez un responsable capable de vous diriger dans la conduite du changement de votre site web et assurer sa pérénité.</p>
     </div>
   </div>
   <div class="last-articles">
