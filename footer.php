5db103dba4fe719876552c90c3a38e6e5a488618
skullmasher <skullmasher@heartlessgaming.com>
2016-11-07 19:10:52 +0100

    Bigger gutter on footer autogrid when using tablet

--- footer.php
+++ footer.php
@@ -12,7 +12,7 @@
 ?>
 </div><!-- #content -->
 <footer class="page-footer band--darkgrey flex-container-v" role="contentinfo">
-    <p class="nota-bene o-box o-box--medium txtcenter"><strong>Skullmasher.io</strong> est une société basée en France affilié au régime auto-entrepreneur. <strong>Numéro siren : 793553488</strong></p>
+    <p class="nota-bene o-box o-box--medium txtcenter"><strong>Skullmasher.io</strong> est une société affilié au régime auto-entrepreneur. <strong>Numéro siren : 793553488</strong></p>
     <div class="autogrid has-gutter o-wrapper o-wrapper--large">
       <div class="keypages page-footer__column o-box">
         <?php get_sidebar('keypages'); ?>
