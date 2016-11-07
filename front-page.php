ac28a82df2555e5a4448423eaa21fc53525e1e2c
skullmasher <skullmasher@heartlessgaming.com>
2016-11-07 19:02:13 +0100

    Wrapper around the promotional-skills

--- front-page.php
+++ front-page.php
@@ -26,34 +26,36 @@ $recent_posts = wp_get_recent_posts($recent_posts_args, OBJECT);
 
 get_header(); ?>
 <main role="main">
-  <div class="promotional-skills band band--lightgrey">
-    <div class="promotional-skill">
-      <header class="promotional-skill__header o-media">
-        <?php echo file_get_contents(get_template_directory() .'/img/develop.svg') ?>
-        <h2 class="promotional-skill__title o-media-content">Concevoir avec Wordpress</h2>
-      </header>
-      <p class="promotional-skill__text">Dans le monde de la gestion de contenu web, Wordpress est bien souvent la solution la plus maitrisée par les utilisateurs. Si à l'avenir votre site vitrine devient un site e-comerce, Wordpress répondra à ce besoin.</p>
-    </div>
-    <div class="promotional-skill">
-      <header class="promotional-skill__header o-media">
-        <?php echo file_get_contents(get_template_directory() .'/img/share.svg') ?>
-        <h2 class="promotional-skill__title o-media-content">Former et Partager</h2>
-      </header>
-      <p class="promotional-skill__text">Demander une formation sur votre solution Wordpress. Découvrez votre site par la pratique. Le web est composé de technologie libre et open-source en son coeur, le partage des connaissances en fait partis intégrante. Retrouvez toutes les astuces et tutoriaux web sur le blog.</p>
-    </div>
-    <div class="promotional-skill">
-      <header class="promotional-skill__header o-media">
-        <?php echo file_get_contents(get_template_directory() .'/img/develop.svg') ?>
-        <h2 class="promotional-skill__title o-media-content">Développer et Déployer</h2>
-      </header>
-      <p class="promotional-skill__text">Adaptation des maquettes aux écrans responsive pour refleter au mieux la vision du web designer. Dévelopement back-office avec PHP et NodeJS. Application iOS et Android hybride avec Cordova (Phonegap). Instalation et déploiement de votre site web en Python, Ruby, PHP, NodeJS sur Linux. Contactez moi pour dicuter de votre projet.</p>
-    </div>
-    <div class="promotional-skill">
-      <header class="promotional-skill__header o-media">
-        <?php echo file_get_contents(get_template_directory() .'/img/server.svg') ?>
-        <h2 class="promotional-skill__title o-media-content">Maintenir des Solutions Web</h2>
-      </header>
-      <p class="promotional-skill__text">Bien trop souvent des utilisateurs se retrouve seul face à la gestion de leur site web. Tous logiciel informatique à besoin d'une personne responsable de sa maintenance. Avant qu'il ne soit trop tard choisissez un responsable capable de vous diriger dans la conduite du changement de votre site web et assurer sa pérénité.</p>
+  <div class="band band--lightgrey">
+    <div class="promotional-skills o-wrapper o-wrapper--huge">
+      <div class="promotional-skill">
+        <header class="promotional-skill__header o-media">
+          <?php echo file_get_contents(get_template_directory() .'/img/develop.svg') ?>
+          <h2 class="promotional-skill__title o-media-content">Concevoir avec Wordpress</h2>
+        </header>
+        <p class="promotional-skill__text">Dans le monde de la gestion de contenu web, Wordpress est bien souvent la solution la plus maitrisée par les utilisateurs. Si à l'avenir votre site vitrine devient un site e-comerce, Wordpress répondra à ce besoin.</p>
+      </div>
+      <div class="promotional-skill">
+        <header class="promotional-skill__header o-media">
+          <?php echo file_get_contents(get_template_directory() .'/img/share.svg') ?>
+          <h2 class="promotional-skill__title o-media-content">Former et Partager</h2>
+        </header>
+        <p class="promotional-skill__text">Demander une formation sur votre solution Wordpress. Découvrez votre site par la pratique. Le web est composé de technologie libre et open-source en son coeur, le partage des connaissances en fait partis intégrante. Retrouvez toutes les astuces et tutoriaux web sur le blog.</p>
+      </div>
+      <div class="promotional-skill">
+        <header class="promotional-skill__header o-media">
+          <?php echo file_get_contents(get_template_directory() .'/img/develop.svg') ?>
+          <h2 class="promotional-skill__title o-media-content">Développer et Déployer</h2>
+        </header>
+        <p class="promotional-skill__text">Adaptation des maquettes aux écrans responsive pour refleter au mieux la vision du web designer. Dévelopement back-office avec PHP et NodeJS. Application iOS et Android hybride avec Cordova (Phonegap). Instalation et déploiement de votre site web en Python, Ruby, PHP, NodeJS sur Linux. Contactez moi pour dicuter de votre projet.</p>
+      </div>
+      <div class="promotional-skill">
+        <header class="promotional-skill__header o-media">
+          <?php echo file_get_contents(get_template_directory() .'/img/server.svg') ?>
+          <h2 class="promotional-skill__title o-media-content">Maintenir des Solutions Web</h2>
+        </header>
+        <p class="promotional-skill__text">Bien trop souvent des utilisateurs se retrouve seul face à la gestion de leur site web. Tous logiciel informatique à besoin d'une personne responsable de sa maintenance. Avant qu'il ne soit trop tard choisissez un responsable capable de vous diriger dans la conduite du changement de votre site web et assurer sa pérénité.</p>
+      </div>
     </div>
   </div>
   <div class="content-sidebar o-wrapper">
