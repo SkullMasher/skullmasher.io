aa18c059690e14b94b36e7f52d5d06655cfb7d45
SkullMasher <skullm4sher@gmail.com>
2017-02-07 15:29:21 +0100

    Does not print styles if there's no post image

--- template-parts/content-blog.php
+++ template-parts/content-blog.php
@@ -6,8 +6,9 @@
  *
  * @package skullmasher.io
  */
+
 ?>
-<article id="post-<?php the_ID(); ?>" <?php post_class('post-list__item'); ?> style="background-image: url('<?php the_post_thumbnail_url() ?>');">
+<article id="post-<?php the_ID(); ?>" <?php post_class('post-list__item'); ?> <?php if (has_post_thumbnail()) { echo 'style="background-image:url(\''; the_post_thumbnail_url(); echo '\');"';} ?>>
     <?php the_title( '<h2 class="post-list__title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h2>' ); ?>
     <div class="post-list__meta flex-container">
       <span class="post-list__publish-date"><?php skullmasher_io_post_publish_date(); ?></span>
