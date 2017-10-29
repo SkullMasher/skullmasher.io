f47ca545906a4339a76e327c11ddd75473fc498a
SkullMasher <skullm4sher@gmail.com>
2017-10-29 03:30:17 +0100

    :skull: Load jspdf on devis page

--- functions.php
+++ functions.php
@@ -166,6 +166,10 @@ function skullmasher_io_scripts() {
 
   wp_enqueue_script( 'skullmasher-io-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20151215', true );
 
+  if (get_the_title() === 'Devis') {
+    wp_enqueue_script( 'skullmasher-io-jspdf', get_template_directory_uri() . '/js/jspdf.min.js', array(), '20171029', true );
+  }
+
   if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
     wp_enqueue_script( 'comment-reply' );
   }
