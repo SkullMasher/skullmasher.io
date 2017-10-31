4b87e8d2edeecce3381a482d5de543ba9d9285b5
SkullMasher <skullm4sher@gmail.com>
2017-10-31 14:01:00 +0100

    :skull: Corercting bad french. Do you even France bro ?

--- template-parts/content-devis.php
+++ template-parts/content-devis.php
@@ -11,17 +11,17 @@
 
 <article id="post-<?php the_ID(); ?>" <?php post_class('o-box o-wrapper o-wrapper--small'); ?>>
 	<header class="page__header">
-    <h1 class="page__title">Deviser un site vitrine ou blog idéal pour les particuliers et entreprises local</h1>
-    <p>Design sur mesure, compatible téléphonne et tablette.</p>
+    <h1 class="page__title">Deviser un site vitrine ou blog, pour les particuliers et les entreprises locales.</h1>
+    <p>Design sur mesure, compatible téléphone et tablette.</p>
 	</header>
 
 	<div class="page-devis">
     <div class="devis-steps flex-container-v">
       <h2>1. Maquette de pages</h2>
-      <p class="devis-question">Combien de maquette de page avez-vous besoin ?</p>
+      <p class="devis-question">Combien de page avez-vous besoin ?</p>
       <div class="devis-tips o-media o-wrapper o-wrapper--tiny">
         <figure class="o-media-figure"><?php echo file_get_contents(get_template_directory() .'/img/light-bulb.svg')?></figure>
-        <p class="o-media-content">Vous pouvez réutiliser la maquette de la page contact sur la page à propos et toute les autres pages. Comprend au minumum une maquette pour la page d'accueil et une maquette pour toutes les autres pages. <span class="animation-showoff">Animation dynamique comprise !</span></p>
+        <p class="o-media-content">Vous pouvez réutiliser la maquette de la page contact sur la page à propos et toute les autres pages. Comprend au minimum une maquette pour la page d'accueil et une maquette pour toutes les autres pages. <span class="animation-showoff">Animation dynamique comprise !</span></p>
       </div>
       <div class="devis-choices flex-container-v o-wrapper o-wrapper--centered">
         <input id="designCount" name="designCount" type="range" min="2" max="10" value="2">
@@ -34,7 +34,7 @@
       <p class="devis-question">Avez-vous un logo ?</p>
       <div class="devis-tips o-media o-wrapper o-wrapper--tiny">
         <figure class="o-media-figure"><?php echo file_get_contents(get_template_directory() .'/img/light-bulb.svg')?></figure>
-        <p class="o-media-body">Nous pouvons vous proposer Jusqu'à 3 proposition de logos.</p>
+        <p class="o-media-body">Je peux vous proposer jusqu'à trois design de logos différents.</p>
       </div>
       <div class="devis-choices o-wrapper o-wrapper--tiny">
         <button class="btn btn--rounded ">OUI</button>
@@ -51,7 +51,7 @@
           <label for="flyer" class="toggle-label">Toggle</label>
           <p class="devis-toggle__label">J'ai besoin d'un flyer</p>
         </div>
-        <p><small>Frait d'impression non pris en charge.</small></p>
+        <p><small>Frais d'impression non pris en charge.</small></p>
       </div>
 
     </div>
@@ -74,7 +74,7 @@
       <p></p>
       <div class="devis-tips o-media o-wrapper o-wrapper--tiny">
         <figure class="o-media-figure"><?php echo file_get_contents(get_template_directory() .'/img/light-bulb.svg')?></figure>
-        <p class="o-media-content">Comprend l'ajout de la fonctionalité avec le design et le code d'un modèle de mail sur mesure.</p>
+        <p class="o-media-content">Comprend l'ajout de la fonctionnalité avec le design et le code d'un modèle de mail sur mesure.</p>
       </div>
       <div class="devis-choices o-wrapper o-wrapper--tiny">
         <button class="btn btn--rounded btn--disabled">OUI</button>
@@ -86,7 +86,7 @@
       <p class="devis-question">Souhaitez-vous une journée de formation pour apprendre à gérer votre site ?</p>
       <div class="devis-tips o-media o-wrapper o-wrapper--tiny">
         <figure class="o-media-figure"><?php echo file_get_contents(get_template_directory() .'/img/light-bulb.svg')?></figure>
-        <p class="o-media-content">Comprend 30€ de frait de déplacement à Paris et benlieue. Possibilité de faire la formation en ligne (skype, discord...)</p>
+        <p class="o-media-content">Comprend 30€ de frais de déplacement à Paris et banlieue. Possibilité de faire la formation en ligne (skype, discord...)</p>
       </div>
       <div class="devis-choices o-wrapper o-wrapper--tiny">
         <button class="btn btn--rounded btn--disabled">OUI</button>
@@ -106,12 +106,17 @@
       <p class="devis-question">Avez vous besoin d'une personne responsable de la maintenance du site ?</p>
       <div class="devis-tips o-media o-wrapper o-wrapper--tiny">
         <figure class="o-media-figure"><?php echo file_get_contents(get_template_directory() .'/img/light-bulb.svg')?></figure>
-        <p class="o-media-content">Tout logiciel informatique à besoin d'une personne responsable de sa maintenance. Avant qu'il ne soit trop tard choisissez un responsable capable de vous diriger à la gestion de votre site web et assurer sa pérénitée.
+        <p class="o-media-content">Tout logiciel informatique à besoin d'une personne responsable de sa maintenance. Avant qu'il ne soit trop tard, choisissez un responsable capable gérer votre web et assurer sa pérénitée. En savoir plus sur l'offre de maintenance.
 <a href="#">En savoir plus sur la l'offre de maintenance</a>.</p>
       </div>
       <div class="devis-choices o-wrapper o-wrapper--tiny">
         <button class="btn btn--rounded btn--disabled">OUI</button>
         <button class="btn btn--rounded">NON</button>
+      </div>
+      <div class="devis-toggle">
+        <input type="checkbox" id="flyer" class="toggle">
+        <label for="flyer" class="toggle-label">Toggle</label>
+        <p class="devis-toggle__label">Je souhaite que vous soyez responsable de la publication et la mise en forme des nouvelles pages et nouveaux article sur mon site.</p>
       </div>
     </div>
     <div class="devis-steps">
@@ -130,26 +135,26 @@
       <h2>FAQ</h2>
       <h3>Combien de temps pour développer mon site ? </h3>
       <ul>
-        <li>Design: 2 à 3 jours de par page avec retour.</li>
-        <li>Logo: 2 à 3 jours avec retour.</li>
-        <li>Dévelopement des pages: 2 à 3 jours de code par page.</li>
-        <li>Newsletter: 4 jours. Design et intégration avec retour.</li>
-        <li>Intégration d'un réseaux social/fil d'actualité: Une demi journée.</li>
-        <li>Recommandation configuration nom de domaine et hébergeur: une demi journée.</li>
-        <li>Déploiement du site, test et retour: 1 à 2 jours.</li>
+        <li>Design : 2 à 3 jours de par page avec retour.</li>
+        <li>Logo : 2 à 3 jours avec retour.</li>
+        <li>Dévelopement des pages : 2 à 3 jours de code par page.</li>
+        <li>Newsletter : 4 jours. Design et intégration avec retour.</li>
+        <li>Intégration d'un réseaux social/fil d'actualité : Une demi-journée.</li>
+        <li>Recommandation configuration nom de domaine et hébergeur : une demi journée.</li>
+        <li>Déploiement du site, tests et retour : 1 à 2 jours.</li>
       </ul>
-      <h3>Comment puis-je réduire le temps de dévellopement de mon projet web ?</h3>
-      <p>Les réponses à ces quelques question m'aide à réduire le temps de dévellopement de votre projet web mais aussi de corespondre plus précisement à vos attentes.</p>
-      <ul>
-        <li>Quelles sont les sites web qui vous inspire ?</li>
-        <li>Avez-vous déjà des élements graphique (photo, pictogramme...) ?</li>
+      <h3>Comment puis-je réduire le temps de développement de mon projet web ?</h3>
+      <p>Les réponses à ces quelques questions m'aide à réduire le temps de dévellopement de votre projet web et à correspondre précisément à vos attentes.</p>
+      <ol>
+        <li>Quelles sont les sites web qui vous inspirent ?</li>
+        <li>Avez-vous déjà des éléments graphiques (photo, pictogramme...) ?</li>
         <li>Avez-vous déjà rédigé votre contenu textuel ?</li>
-        <li>Quelles sont vos mots clef ?</li>
-      </ul>
+        <li>Quels sont vos mots clés ?</li>
+      </ol>
       <h3>Mon site sera t-il présent sur google (SEO) ?</h3>
-      <p>Nous établirons ensemble une stragégie d'url et de titre de page autour de vos mots clef principaux. Le SEO de votre site sera optimisé par le plugin wordpress "the SEO Framework".</p>
-      <h3>Comment ce passe la gestion de mon site ?</h3>
-      <p>Votre site est développer avec Wordpress, le gestionnaire de contenu le plus utilisé sur le web et souvent celui qui ai le plus maitrisé par les utilisateurs.</p>
+      <p>Nous établirons ensemble une stragégie d'url et de titre de page autour de vos mots clef principaux. Le SEO de votre site sera optimisé par le plugin wordpress "The SEO Framework".</p>
+      <h3>Comment se passe la gestion de mon site ?</h3>
+      <p>Votre site est développé avec Wordpress, le gestionnaire de contenu le plus utilisé sur le web et souvent celui qui est le plus maîtrisé par les utilisateurs.</p>
     </div>
     <?php
       the_content();
