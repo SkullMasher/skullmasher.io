9c33daab46c6d0892ff6030689e7ff592e947f95
SkullMasher <skullm4sher@gmail.com>
2017-11-15 17:50:07 +0100

    :skull: Added text to label for accessibility purpose. Might redo that later.

--- template-parts/content-devis.php
+++ template-parts/content-devis.php
@@ -68,14 +68,14 @@
           <div class="devis-switch">
             <div class="devis-switch__input">
               <input type="checkbox" id="businessCard" class="toggle" data-choiceid="1" data-optionid="0">
-              <label for="businessCard" class="toggle-label">Toggle</label>
+              <label for="businessCard" class="toggle-label">J'ai besoin d'une carte de visite.</label>
             </div>
             <p class="devis-switch__label">J'ai besoin d'une carte de visite</p>
           </div>
           <div class="devis-switch">
             <input type="checkbox" id="flyer" class="toggle" data-choiceid="1" data-optionid="0">
-            <label for="flyer" class="toggle-label">Toggle</label>
-            <p class="devis-switch__label">J'ai besoin d'un flyer</p>
+            <label for="flyer" class="toggle-label">J'ai besoin d'un flyer.</label>
+            <p class="devis-switch__label">J'ai besoin d'un flyer.</p>
           </div>
           <p><small>Frais d'impression non pris en charge.</small></p>
         </div>
@@ -91,7 +91,7 @@
           <div class="devis-switch">
             <div class="devis-switch__input">
               <input type="checkbox" id="socialNetwork" class="toggle" data-choiceid="2" data-optionid="0">
-              <label for="socialNetwork" class="toggle-label">Toggle</label>
+              <label for="socialNetwork" class="toggle-label">Je souhaite également intégrer le fil d'actualité, les avis ou les recommendations d'un de mes réseaux sociaux.</label>
             </div>
             <p class="devis-switch__label">Je souhaite également intégrer le fil d'actualité, les avis ou les recommendations d'un de mes réseaux sociaux.</p>
           </div>
@@ -145,7 +145,7 @@
           <div class="devis-switch">
             <div class="devis-switch__input">
               <input type="checkbox" id="maintainWebsite" class="toggle" data-choiceid="6" data-optionid="0">
-              <label for="maintainWebsite" class="toggle-label">Toggle</label>
+              <label for="maintainWebsite" class="toggle-label">Je souhaite que vous soyez responsable de la publication et la mise en forme des nouvelles pages et nouveaux article sur mon site.</label>
             </div>
             <p class="devis-switch__label">Je souhaite que vous soyez responsable de la publication et la mise en forme des nouvelles pages et nouveaux article sur mon site.</p>
           </div>
