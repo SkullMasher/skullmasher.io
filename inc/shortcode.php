680f2804e3b576239012ef52ae333d07654ed48a
SkullMasher <skullm4sher@gmail.com>
2019-01-23 17:29:34 +0100

    :skull: Update my contact text

--- inc/shortcode.php
+++ inc/shortcode.php
@@ -12,7 +12,7 @@ function whoami_shortcode() {
   $about_url = site_url() . '/a-propos';
 
 $whoami = <<<EOL
-<p class="whoami">Florian "Skullmasher" Ledru, 24ans, freelance depuis juin 2016. J'exerce en région parisienne et partout sur la toile. Passionné d'informatique, j'ai choisis de faire du web mon travail car c'est un outil très accessible et construit à partir de technologies libre en son coeur. Pour en savoir plus consulter la page <a href="$about_url">à propos</a>.</p>
+<p class="whoami">Florian "Skullmasher" Ledru, 26ans, freelance depuis juin 2016. Passionné d'informatique, j'ai choisis de faire du web mon travail car c'est un outil très accessible et construit à partir de technologies libre en son coeur. Pour en savoir plus consulter la page <a href="$about_url">à propos</a>.</p>
 EOL;
 
   return $whoami;
@@ -44,4 +44,3 @@ EOL;
   return $social;
 }
 add_shortcode( 'worktogether', 'worktogether_shortcode' );
-
